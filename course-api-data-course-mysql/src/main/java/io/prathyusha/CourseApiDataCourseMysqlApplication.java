90b7375a0ef5d9126d857de2f9b06305ddf49570
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-18 12:23:27 +0530

    Add files via upload

---
+++ course-api-data-course-mysql/src/main/java/io/prathyusha/CourseApiDataCourseMysqlApplication.java
@@ -0,0 +1,13 @@
+package io.prathyusha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataCourseMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataCourseMysqlApplication.class, args);
+	}
+
+}
