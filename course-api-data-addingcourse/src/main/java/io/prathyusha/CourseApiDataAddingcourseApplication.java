f295f3822845c6e4fa153b5340fb64f0c5d6900a
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-18 06:09:50 +0530

    Add files via upload

---
+++ course-api-data-addingcourse/src/main/java/io/prathyusha/CourseApiDataAddingcourseApplication.java
@@ -0,0 +1,13 @@
+package io.prathyusha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataAddingcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataAddingcourseApplication.class, args);
+	}
+
+}
---
+++ course-api-data-addingcourse/src/test/java/io/prathyusha/CourseApiDataAddingcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prathyusha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataAddingcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
