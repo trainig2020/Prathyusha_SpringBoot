93fa02c7da7edb9a5acccd214fdb9c3797ae446f
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-18 12:05:42 +0530

    Add files via upload

---
+++ course-api-data-mysql/src/test/java/io/prathyusha/CourseApiDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prathyusha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
